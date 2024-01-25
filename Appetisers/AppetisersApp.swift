0f9332a7dac5e954bd66fba58db519154f765e45
shomil03 <shomil02020@gmail.com>
2024-01-25 22:28:04 +0530

    Initial Commit

---
+++ Appetisers/AppetisersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetisersApp.swift
+//  Appetisers
+//
+//  Created by Shomil Singh on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetisersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
